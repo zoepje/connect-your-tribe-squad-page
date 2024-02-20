53f0ca54de4b84be779b670eaf624340f71f6d62
zoepje <zoe.penner@hva.nl>
2024-02-20 15:27:59 +0100

    client js delete

--- public/scripts/main.js
+++
@@ -1,9 +0,0 @@
-const images = ["/assets/Type_Fire.webp", "/assets/Type_Water.webp", "/assets/Type_Grass.webp"];
-
-const outElem = document.querySelector('.images');
-
-images.forEach(function (path) {
-  const img = document.createElement("img");
-  img.src = path;
-  outElem.appendChild(img);
-});
