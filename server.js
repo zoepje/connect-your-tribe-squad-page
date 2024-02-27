db52f1af905a71ef1f8f4df66b2e39cbf3cc9a5a
zoepje <zoe.penner@hva.nl>
2024-02-27 12:23:42 +0100

    comments toevoegen aan API

--- server.js
+++ server.js
@@ -103,15 +103,64 @@ app.get('/squad/:id', function (request, response) {
 app.get('/person/:id', function (request, response) {
   // Gebruik de request parameter id en haal de juiste personen uit de squad uit de WHOIS API op
   fetchJson(apiUrl + 'person/' + request.params.id).then((person) => {
-
+    // Het custom field is een String, dus die moeten we eerst omzetten (= parsen)
+    // naar een Object, zodat we er mee kunnen werken
+    try {
+      person.data.custom = JSON.parse(person.data.custom)
+    } catch (error) {}
     // Render squad.ejs uit de views map en geef de opgehaalde data mee als variable, genaamd squad
-    response.render('person', {person: person.data, squad: squadData.data, styles: typeCss, comments: comments})
+    response.render('person', {person: person.data, squad: squadData.data, styles: typeCss})
   })
 })
 
-// Maak een POST route voor person
-app.post('/person/:id', function (request, response) {
-  comments.push(request.body.comment)
-  // Er is nog geen afhandeling van POST, redirect naar GET op /
-  response.redirect(303, '/person/' + request.params.id)
+// Als we vanuit de browser een POST doen op de detailpagina van een persoon
+app.post('/person/:id', function(request, response) {
+  // Stap 1: Haal de huidige data op, zodat we altijd up-to-date zijn, en niks weggooien van anderen
+  // Haal eerst de huidige gegevens voor deze persoon op, uit de WHOIS API
+  fetchJson(apiUrl + 'person/' + request.params.id).then((apiResponse) => {
+    // Het custom field is een String, dus die moeten we eerst
+    // omzetten (= parsen) naar een Object, zodat we er mee kunnen werken
+    try {
+      apiResponse.data.custom = JSON.parse(apiResponse.data.custom)
+    } catch (error) {
+      apiResponse.data.custom = {}
+    }
+
+    // Stap 2: Gebruik de data uit het formulier
+    // Deze stap zal voor iedereen net even anders zijn, afhankelijk van de functionaliteit
+
+    // Controleer eerst welke actie is uitgevoerd, aan de hand van de submit button
+    // Dit kan ook op andere manieren, of in een andere POST route
+    if (request.body.actie == 'sent') {
+      // Als het custom object nog geen messages Array als eigenschap heeft, voeg deze dan toe
+      if (!apiResponse.data.custom.comments) {
+        apiResponse.data.custom.comments = []
+      }
+
+      // Voeg een nieuwe message toe voor deze persoon, aan de hand van het bericht uit het formulier
+      apiResponse.data.custom.comments.push(request.body.comment)
+
+    } else if (request.body.vote == 'upvote') {
+      apiResponse.data.custom.vote = true
+
+    } else if (request.body.vote == 'donwvote') {
+      apiResponse.data.custom.vote = false
+    }
+
+    // Stap 3: Sla de data op in de API
+    // Voeg de nieuwe lijst messages toe in de WHOIS API,
+    // via een PATCH request
+    fetch(apiUrl + 'person/' + request.params.id, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        custom: apiResponse.data.custom
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    }).then((patchResponse) => {
+      // Redirect naar de persoon pagina
+      response.redirect(303, '/person/' + request.params.id)
+    })
+  })
 })
